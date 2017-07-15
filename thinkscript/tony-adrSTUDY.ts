25ebcd2e819ac8ef38e45fb4d3ca30a9f6392bf4
tony thell <tonythell@gmail.com>
2017-07-14 20:51:49 -0500

    add adr script.
    The original source and inspiration for this code comes from
    https://github.com/RyanHendricks/Thinkorswim-Indicators.

---
+++ thinkscript/tony-adrSTUDY.ts
@@ -0,0 +1,17 @@
+#
+# average daily range
+#
+# original source for this comes from https://github.com/RyanHendricks/Thinkorswim-Indicators
+#
+
+def len = 2;
+
+def dayHigh = DailyHighLow(AggregationPeriod.DAY, len, 0, no).dailyhigh;
+def dayLow = DailyHighLow(AggregationPeriod.DAY, len, 0, no).DailyLow;
+
+def ADR_high = (dayHigh + dayHigh[1] + dayHigh[2] + dayHigh[3] + dayHigh[4]) / 5;
+def ADR_low = (dayLow + dayLow[1] + dayLow[2] + dayLow[3] + dayLow[4]) / 5;
+
+plot ADR_H = ADR_high;
+plot ADR_L = ADR_low;
+
